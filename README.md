2bee0b216e70bb8c96b719a6e6f468d2c9fefe83
brunomedve <bruno_medve@outlook.com>
2023-06-10 11:31:29 -0300

    Segundo commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Criado a tela de login do projeto promointer 
+
+10/06/2023 - App Atualizado com mais funcionalidades, e criado novas telas
---
+++ TelaPromoInter/src/Aplication/ModeloTelaPrincipal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aplication;
+
+/**
+ *
+ * @author bruno
+ */
+public class ModeloTelaPrincipal {
+    
+    private String tituloOferta;
+
+    public String getTituloOferta() {
+        return tituloOferta;
+    }
+
+    public void setTituloOferta(String tituloOferta) {
+        this.tituloOferta = tituloOferta;
+    }
+    
+    
+}
